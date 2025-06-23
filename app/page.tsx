d1ab2c2374953ad4ddcc5f70bc540aac84031be3
Eshaank08 <113687136+Eshaank08@users.noreply.github.com>
2025-06-23 13:59:17 +0200

    Add real URLs to links and set up custom fonts

--- app/page.tsx
+++ app/page.tsx
@@ -3,60 +3,58 @@ export default function Component() {
     <div className="min-h-screen bg-[#f5f5dc] flex items-center justify-center px-8">
       <div className="max-w-2xl space-y-6">
         {/* Name */}
-        <h1 className="text-black font-normal text-lg">Eshaan Kansal</h1>
+        <h1 className="text-black font-normal text-lg gazette-font">Eshaan Kansal</h1>
 
         {/* Main description */}
-        <p className="text-black leading-relaxed">
+        <p className="text-black leading-relaxed gazette-font">
           21 years old, INTJ. Indian, living in Germany.
           <br />
           I am the marketing guy, a bit funny and mostly anxious.
         </p>
 
-        <p className="text-black leading-relaxed">
-          I used to play football, now I train like a hybrid athlete and eat burgers for cheatmeals.
-        </p>
-        <p className="text-black leading-relaxed">This is what I am doing</p>
+        <p className="text-black leading-relaxed larsseit-font">I used to play football, now I train like a hybrid athlete and eat burgers for cheatmeals.</p>
+        <p className="text-black leading-relaxed larsseit-font">This is what I am doing</p>
 
-        <p className="text-black leading-relaxed">Building the go-to platform around consulting.</p>
+        <p className="text-black leading-relaxed larsseit-font">Building the go-to platform around consulting.</p>
 
-        <p className="text-black leading-relaxed">
+        <p className="text-black leading-relaxed larsseit-font">
           We help people connect with experts all around the world so they can get 1:1 mentorship.{" "}
-          <a href="#" className="underline hover:no-underline">
+          <a href="https://www.socialease.com" className="underline hover:no-underline italic larsseit-font">
             Socialease
           </a>
         </p>
 
-        <p className="text-black leading-relaxed">
+        <p className="text-black leading-relaxed larsseit-font">
         We also help people become experts, and we advise other small and medium sized businesses. If you want to get in touch regarding that{" "}
-          <a href="#" className="underline hover:no-underline">
+          <a href="https://cal.com/eshaan-kansal" className="underline hover:no-underline italic">
             Connect with me here.
           </a>
         </p>
 
-        <p className="text-black leading-relaxed">
+        <p className="text-black leading-relaxed larsseit-font">
           I also created an AI chatbot which can mentor you for free if you don&apos;t like talking to someone.{" "}
-          <a href="#" className="underline hover:no-underline">
+          <a href="https://socialease.in" className="underline hover:no-underline italic">
             Socialease AI 
           </a>
         </p>
 
-        <p className="text-black leading-relaxed">
+        <p className="text-black leading-relaxed larsseit-font">
           You will find me making vlogs on YouTube, sharing my perspectives and documenting my journey on all these
           platforms here:
         </p>
 
         {/* Links */}
-        <div className="space-x-4 text-black">
-          <a href="#" className="underline hover:no-underline">
+        <div className="flex flex-wrap gap-4 text-black larsseit-font">
+          <a href="https://www.youtube.com/@eshaankansal/videos" className="underline hover:no-underline italic">
             youtube
           </a>
-          <a href="#" className="underline hover:no-underline">
+          <a href="https://x.com/eshaankansal" className="underline hover:no-underline italic">
             twitter
           </a>
-          <a href="#" className="underline hover:no-underline">
+          <a href="https://www.linkedin.com/in/eshaan-kansal-277876241/" className="underline hover:no-underline italic">
             linkedin
           </a>
-          <a href="#" className="underline hover:no-underline">
+          <a href="https://www.instagram.com/eshaan.kansal" className="underline hover:no-underline italic">
             instagram
           </a>
         </div>
