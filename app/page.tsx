5d52f1790a82909a64c80a6028df22af57372ae1
Eshaank08 <113687136+Eshaank08@users.noreply.github.com>
2025-07-06 01:50:32 +0200

    "Fixed some data"

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,17 @@ export default function Component() {
           21 years old, INTJ-P. 
           <br />
           Indian, living in Germany. I call myself the jack of all trades.
-          I am the marketing guy, a bit funny and mostly anxious.
+          <br />
+          I am the marketing guy. I yap about tech, business or anything tbh.
         </p>
 
         <p className="text-black leading-relaxed text-base md:text-lg font-playfair-display">I used to play football, now I train like a hybrid athlete and eat burgers for cheatmeals.</p>
         <p className="text-black leading-relaxed text-base md:text-lg font-playfair-display">This is what I am doing</p>
 
         <p className="text-black leading-relaxed text-base md:text-lg font-playfair-display">
-          Building a marketplace for you to connect with experts, or show your products to the whole world,  or talk with an AI mentor to help yourself out.{" "}
+          Founder - Socialease
+          A marketplace for you to connect with certified experts within minutes across various categories.
+          We have 1800+ people in the waitlist.{" "}
           <a 
             href="https://www.socialease.space" 
             className="italic font-bold no-underline hover:opacity-80 text-base md:text-lg font-playfair-display"
@@ -32,18 +35,18 @@ export default function Component() {
         </p>
 
         <p className="text-black leading-relaxed text-base md:text-lg font-playfair-display">
-          You can just pay me too, if you want help in building apps or with marketing or anyother things haha.{" "}
+          If you want help in building apps or with marketing or any other things.(Only serious work){" "}
           <a 
             href="https://calendly.com/eshaankansal0/general-meeting" 
             className="italic font-bold no-underline hover:opacity-80 text-base md:text-lg"
             onClick={() => trackBusinessClick('Personal Consulting', 'https://calendly.com/eshaankansal0/general-meeting')}
           >
-            Connect with me here
+            Connect with me here.
           </a>
         </p>
 
         <p className="text-black leading-relaxed text-base md:text-lg font-playfair-display">
-        We also help people become experts, and we advise other small and medium sized businesses. If you want to get in touch regarding that{" "}
+        We also help people become certified experts, and we advise other small and medium sized businesses.{" "}
           <a 
             href="https://cal.com/eshaan-kansal" 
             className="italic font-bold no-underline hover:opacity-80 text-base md:text-lg"
