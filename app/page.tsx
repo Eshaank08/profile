ffd27f965e9af32be70fb8aa782fb1a1d8650396
Eshaank08 <113687136+Eshaank08@users.noreply.github.com>
2025-08-23 01:05:28 +0200

    Made some changes

--- app/page.tsx
+++ app/page.tsx
@@ -90,6 +90,16 @@ export default function Component() {
                 <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
               </svg>
             </a>
+            <a 
+              href="https://instagram.com/eshaan.kansal" 
+              onClick={() => trackSocialClick('Instagram', 'https://instagram.com/eshaan.kansal')}
+              className={`${darkMode ? 'text-white' : 'text-gray-800'} hover:opacity-80`}
+              aria-label="Instagram"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+              </svg>
+            </a>
           </div>
         </div>
 
@@ -120,7 +130,7 @@ export default function Component() {
                   </div>
                   <div>
                     <h3 className="font-medium">Founder - Socialease</h3>
-                    <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>A platfrorm for you to sell your skills and make money. 1800+ people in the waitlist.</p>
+                    <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>An online campus for business enthusiasts and skilled professionals to learn and earn. 1800+ people in the waitlist.</p>
                   </div>
                 </div>
               </div>
@@ -128,25 +138,27 @@ export default function Component() {
             
             {/* Work Experience Item 2 */}
             <a 
-              href="/work" 
-              onClick={() => trackBusinessClick('work', '/work')}
+              href="https://cal.com/eshaan-kansal/quick-chat" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              onClick={() => trackBusinessClick('magicLabs', 'https://cal.com/eshaan-kansal/quick-chat')}
               className="block hover:opacity-80 transition-opacity"
             >
               <div className="flex justify-between items-start">
                 <div className="flex items-start">
                   <div className="w-12 h-12 rounded-full mr-3 flex-shrink-0 overflow-hidden">
                     <Image 
-                      src="/images/work/wolf.jpg" 
-                      alt="Personal Branding" 
-                      width={48} 
-                      height={48}
+                      src="/images/work/wolf.png" 
+                      alt="Magic Labs" 
+                      width={100} 
+                      height={100}
                       quality={100}
                       className="w-full h-full object-cover"
                     />
                   </div>
                   <div>
-                    <h3 className="font-medium">Personal Branding + Marketing</h3>
-                    <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>Consulted and worked with over 150+ people. Check out my work here.</p>
+                    <h3 className="font-medium">Magic Labs</h3>
+                    <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>We develop AI products/agents for people along with go to marketing plans to drive growth. Book a call now.</p>
                   </div>
                 </div>
               </div>
@@ -184,39 +196,12 @@ export default function Component() {
                         className="ml-1 inline-block"
                       />
                     </h3>
-                    <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>Making vlogs here.</p>
+                    <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>This is where I yap about Business, tech, marketing and my life.</p>
                   </div>
                 </div>
               </div>
             </a>
             
-            {/* Work Experience Item 4 */}
-            <a 
-              href="https://instagram.com/eshaan.kansalts" 
-              target="_blank" 
-              rel="noopener noreferrer"
-              onClick={() => trackSocialClick('business_instagram', 'https://instagram.com/eshaan.kansalts')}
-              className="block hover:opacity-80 transition-opacity"
-            >
-              <div className="flex justify-between items-start">
-                <div className="flex items-start">
-                  <div className="w-12 h-12 rounded-full mr-3 flex-shrink-0 overflow-hidden">
-                    <Image 
-                      src="/images/work/tech.png" 
-                      alt="Business Instagram" 
-                      width={48} 
-                      height={48}
-                      quality={100}
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                  <div>
-                    <h3 className="font-medium">Business Instagram - @eshaan.kansalts</h3>
-                    <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>Here I yap about Business, Tech and AI.(Recently started)</p>
-                  </div>
-                </div>
-              </div>
-            </a> 
              {/* Work Experience Item 5 - Book a call */}
 <a 
   href="https://cal.com/eshaan-kansal/15min" 
@@ -241,7 +226,7 @@ export default function Component() {
       </div>
       <div>
         <h3 className="font-medium">Book a call with me</h3>
-        <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>Connect 1:1 with me (Mentorships, branding, business, etc)(Only serious work)</p>
+        <p className={`${darkMode ? 'text-gray-300' : 'text-gray-600'} text-sm`}>Connect 1:1 with me (Mentorships, branding, business, etc)</p>
       </div>
     </div>
   </div>
